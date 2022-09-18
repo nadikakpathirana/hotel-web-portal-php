bfbdebb70cc6659e71fb9f9791521e63de2223d4
nadika <nadikakpathirana@gmail.com>
2022-09-18 15:20:48 +0530

    Added admin functionality.

---
+++ admin/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Hotel SunView</title>
+
+    <!-- Google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
+
+    <!-- font awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
+
+
+    <!-- animate.css -->
+    <link rel="stylesheet" href="../assets/wow/animate.css" />
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="../images/favicon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+
+<body id="home">
+
+<nav class="navbar" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <div class="col-xs-5 nav-logo"><img src="../images/logo.png" alt="logo" width="45px"></div>
+                <div class="col-xs-7" style="margin-top: 18px; font-size: 16pt; margin-left: -12px; font-family: fantasy;">SunView</div>
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+        </div>
+    </div>
+</nav>
+
