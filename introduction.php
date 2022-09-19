072bd1118924df055c6eb11a641e81775f9e157c
Priyanjith Priyanga <104417527+priyanjih@users.noreply.github.com>
2022-09-19 21:27:25 +0530

    Added introduction.php
    Please review and approve

---
+++ introduction.php
@@ -0,0 +1,21 @@
+<?php include 'header.php';?>
+<div class="container">
+
+       <h1 class="title">Introduction</h1>
+       <div class="row">
+              <div class="col-sm-4"><p>Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1, Long content 1. </p></div>
+              <div class="col-sm-4"><p>Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2, Long content 2. </p></div>
+              <div class="col-sm-4"><p>Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3, Long content 3. </p></div>
+       </div>
+
+
+       <div class="spacer">
+       <div class="embed-responsive embed-responsive-16by9"><iframe  class="embed-responsive-item" src="https://www.youtube.com/embed/5u1WISBbo5I" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+       
+       </div>
+
+
+
+
+</div>
+<?php include 'footer.php';?>
